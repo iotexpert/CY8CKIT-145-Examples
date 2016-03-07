389501864cfe4f821b17b838cec666bdc30588bf
iotexpert <engineer@iotexpert.com>
2016-03-07 07:54:47 -0500

    Added 3xor project

---
+++ SmartIO/3XOR.cydsn/main.c
@@ -0,0 +1,31 @@
+
+#include <project.h>
+
+int main()
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+    SmartIO_Start();
+    
+    CapSense_Start();
+    CapSense_InitializeAllBaselines();
+    CapSense_ScanAllWidgets();
+
+    for(;;)
+    {
+        if(!CapSense_IsBusy())
+        {
+            CapSense_ProcessAllWidgets();
+        
+            LED10_Write(!CapSense_IsWidgetActive(CapSense_BUTTON0_WDGT_ID));
+            LED11_Write(!CapSense_IsWidgetActive(CapSense_BUTTON1_WDGT_ID));
+            LED9_Write(!CapSense_IsWidgetActive(CapSense_BUTTON2_WDGT_ID));
+                      
+            CapSense_UpdateAllBaselines();
+            CapSense_ScanAllWidgets();
+        }
+        
+        P20_Write(!LED9_Read());
+        P21_Write(!LED10_Read());
+        P22_Write(!LED11_Read());
+    }
+}
